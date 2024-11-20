9523f5fc45d9e5280441e69a36c2b6b0bbf339ee
bharathsbk-hub <vinosiva9999@gmail.com>
2024-11-20 05:59:55 +0530

    Delete aws-exports.js

--- aws-exports.js
+++
@@ -1,9 +0,0 @@
-const awsConfig = {
-    Auth: {
-        region: 'us-east-1', // Replace with your AWS region
-        userPoolId: 'us-east-1_Oh0Lq68iE', // Replace with your Cognito User Pool ID
-        userPoolWebClientId: '1r7c7073vrotgl0ilih1m8qa55', // Replace with your Web Client ID
-    }
-};
-
-window.awsConfig = awsConfig; // Expose the config globally for use in your HTML
